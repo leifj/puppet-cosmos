007a697c756d66e4bc924784e67b45a00f2e12d0
Leif Johansson <leifj@sunet.se>
2013-08-28 16:14:47 +0200

    arg madness

--- manifests/kvm.pp
+++ manifests/kvm.pp
@@ -16,11 +16,11 @@ define cosmos::kvm($domain, $ip, $netmask, $resolver, $gateway, $repo, $suite='p
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     timeout => 3600,
     command => "virsh destroy $name || true ; virsh undefine $name || true ; /usr/bin/vmbuilder \
-      kvm ubuntu  -d /var/lib/libvirt/images/$name -m $memory --cpus=$cpus --rootsize=$rootsize \
-      --domain=$domain --bridge=$bridge --ip=$ip --mask=$netmask --gw=$gateway --dns=$resolver \
-      --hostname=$name --ssh-key=/root/.ssh/authorized_keys --suite=$suite --flavour=virtual --libvirt=qemu:///system \
+      kvm ubuntu  -d /var/lib/libvirt/images/$name -m $memory --cpus $cpus --rootsize $rootsize \
+      --domain $domain --bridge $bridge --ip $ip --mask $netmask --gw $gateway --dns $resolver \
+      --hostname $name --ssh-key /root/.ssh/authorized_keys --suite $suite --flavour virtual --libvirt qemu:///system \
       --verbose --firstboot=/tmp/firstboot_${name} --copy=/tmp/files_${name} \
-      --addpkg=openssh-server --addpkg=unattended-upgrades > /tmp/vm-$name-install.log 2>&1 && virsh start $name" ,
+      --addpkg openssh-server --addpkg unattended-upgrades > /tmp/vm-$name-install.log 2>&1 && virsh start $name" ,
     unless => "/usr/bin/test -d /var/lib/libvirt/images/${name}",
   }
 
