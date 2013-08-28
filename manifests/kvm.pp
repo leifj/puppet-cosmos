d67552e549e85bd889120aa92612398209d8c8f5
Leif Johansson <leifj@sunet.se>
2013-08-28 12:18:59 +0200

    add bridge argument to vmbuilder

--- manifests/kvm.pp
+++ manifests/kvm.pp
@@ -5,12 +5,12 @@ define cosmos::kvm($domain, $ip, $netmask, $resolver, $gateway, $bridge='br0', $
   exec { "create_cosmos_vm_${name}":
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     timeout => 3600,
-    command => "virsh destroy $name ; virsh undefine $name ; /usr/bin/vmbuilder \
+    command => "virsh destroy $name || true ; virsh undefine $name || true ; /usr/bin/vmbuilder \
       kvm ubuntu  -d /var/lib/libvirt/images/$name -m $memory --cpus=$cpus --rootsize=$rootsize \
       --domain=$domain --bridge=$bridge --ip=$ip --mask=$netmask --gw=$gateway --dns=$resolver \
       --hostname=$name --ssh-key=/root/.ssh/authorized_keys --libvirt=qemu:///system \
       --verbose --firstboot=/root/bootstrap-cosmos.sh \
-      --copy=/root/cosmos_1.2-2_all.deb --addpkg=openssh-server --addpkg=unattended-upgrades && virsh start $name" ,
+      --copy=/var/lib/libvirt/images/bootstrap --addpkg=openssh-server --addpkg=unattended-upgrades && virsh start $name" ,
     unless => "/usr/bin/test -d /var/lib/libvirt/images/$name",
   }
 }
