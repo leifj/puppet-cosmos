0d0140a31fef4fc0d7c8e9ae325ab6c6f05118f6
Leif Johansson <leifj@sunet.se>
2013-08-28 10:34:18 +0200

    add bridge argument to vmbuilder

--- manifests/kvm.pp
+++ manifests/kvm.pp
@@ -1,13 +1,13 @@
 
 # inspired by http://blogs.thehumanjourney.net/oaubuntu/entry/kvm_vmbuilder_puppet_really_automated
 
-define cosmos::kvm($domain, $ip, $netmask, $resolver, $gateway, $memory='512', $rootsize='20G', $cpus = '1' ) {
+define cosmos::kvm($domain, $ip, $netmask, $resolver, $gateway, $bridge='br0', $memory='512', $rootsize='20G', $cpus = '1' ) {
   exec { "create_cosmos_vm_${name}":
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     timeout => 3600,
     command => "virsh destroy $name ; virsh undefine $name ; /usr/bin/vmbuilder \
       kvm ubuntu  -d /var/lib/libvirt/images/$name -m $memory --cpus=$cpus --rootsize=$rootsize \
-      --domain=$domain --ip=$ip --mask=$netmask --gw=$gateway --dns=$resolver \
+      --domain=$domain --bridge=$bridge --ip=$ip --mask=$netmask --gw=$gateway --dns=$resolver \
       --hostname=$name --ssh-key=/root/.ssh/authorized_keys --libvirt=qemu:///system \
       --verbose --firstboot=/root/bootstrap-cosmos.sh \
       --copy=/root/cosmos_1.2-2_all.deb --addpkg=openssh-server --addpkg=unattended-upgrades && virsh start $name" ,
