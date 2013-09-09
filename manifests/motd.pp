d3ab94e1ed15b3855646495cd099473677354b8b
Leif Johansson <leifj@sunet.se>
2013-09-09 11:27:25 +0200

    fix class

--- manifests/motd.pp
+++ manifests/motd.pp
@@ -1,14 +1,14 @@
 # This manifest is managed using cosmos
 
-class mnt::cosmos {
+class cosmos::cosmos {
    file {'motd':
       ensure   => file,
       path     => '/etc/motd.tail',
       mode     => 0644,
       content  => "
 
-This mashine (${::fqdn}) is managed by hostmaster@mnt.se and 
-is running ${::operatingsystem} ${::operatingsystemrelease} using puppet version ${::puppetversion} and cosmos
+This mashine (${::fqdn}) is is running ${::operatingsystem} ${::operatingsystemrelease} 
+using puppet version ${::puppetversion} and cosmos
 
 "
    }
