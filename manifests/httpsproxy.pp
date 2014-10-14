8c8ce6702408b141653be475c0eff1421eb7fc65
Leif Johansson <leifj@sunet.se>
2014-10-15 00:43:28 +0200

    turn off ssl3

--- manifests/httpsproxy.pp
+++ manifests/httpsproxy.pp
@@ -14,7 +14,8 @@ class cosmos::httpsproxy ($certs = ['/etc/ssl/private/server.pem']) {
       require => Package['ssl-cert'],
    }
    Exec['generate-snakeoil'] -> File['/etc/ssl/private/server.pem']
-   $ssl_ciphers = 'TLSv1.2 TLSv1.1 !SSLv3 !SSLv2 EDH+CAMELLIA EDH+aRSA EECDH+aRSA+AESGCM EECDH+aRSA+SHA384 EECDH+aRSA+SHA256 EECDH +CAMELLIA256 +AES256 +CAMELLIA128 +AES128 !aNULL !eNULL !LOW !3DES !MD5 !EXP !PSK !DSS !RC4 !SEED !ECDSA CAMELLIA256-SHA AES256-SHA CAMELLIA128-SHA AES128-SHA'
+   $ssl_ciphers_dfri = "ECDHE-ECDSA-AES256-GCM-SHA384 ECDHE-RSA-AES256-GCM-SHA384 ECDHE-ECDSA-AES256-SHA384 ECDHE-RSA-AES256-SHA384 ECDHE-ECDSA-AES128-GCM-SHA256 ECDHE-RSA-AES128-GCM-SHA256 ECDHE-ECDSA-AES128-SHA256 ECDHE-RSA-AES128-SHA256 ECDHE-RSA-AES256-SHA DHE-RSA-AES256-SHA DHE-RSA-AES128-SHA AES256-SHA256"
+   $ssl_ciphers_eduid_pfs = "DHE-RSA-CAMELLIA256-SHA DHE-RSA-AES256-SHA DES-CBC3-SHA"
    pound::entry {"$name-httpsproxy-443":
       listen_ip => $ipaddress,
       listen_port => '443',
@@ -22,7 +23,7 @@ class cosmos::httpsproxy ($certs = ['/etc/ssl/private/server.pem']) {
       head_require => 'Host:.*',
       backend_ip => '127.0.0.1',
       backend_port => '80',
-      ssl_ciphers => $ssl_ciphers,
+      ssl_ciphers  => "${ssl_ciphers_dfri} ${ssl_ciphers_eduid_pfs}",
       server_certs => $certs
    }
    include augeas
